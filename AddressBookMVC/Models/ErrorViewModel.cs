9bee8db626ce84920ad6aadf64455d5b47195946
Ford Henley <fordp86@gmail.com>
2022-08-11 10:30:01 -0500

    Initial commit

---
+++ AddressBookMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AddressBookMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
---
+++ AddressBookMVC/Startup.cs
@@ -0,0 +1,12 @@
+﻿using AddressBookMVC.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
